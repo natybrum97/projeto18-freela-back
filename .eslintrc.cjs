850013b02a99cedea94ccf197fd1b808c8dd86a8
Nathália Brum <natybrum.97@gmail.com>
2023-12-09 04:51:06 -0300

    new

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,12 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
+    'import/no-unresolved': 'error',
+    'import/order': 'warn',
+    'import/no-named-as-default-member': 'off',
+    'import/newline-after-import': ['error'],
+    'lines-between-class-members': ['error', 'always'],
+    'prettier/prettier': 'error',
   },
   settings: {
     'import/resolver': { node: {} },
